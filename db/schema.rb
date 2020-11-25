07a80064c45916e75a173981de0acd57d1cb2dfc
alejandrotoledoweb <atoledofr@gmail.com>
2020-11-25 17:42:57 -0500

    add name for user and user_id for posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_25_215824) do
+ActiveRecord::Schema.define(version: 2020_11_25_223856) do
 
   create_table "posts", force: :cascade do |t|
     t.text "post"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
